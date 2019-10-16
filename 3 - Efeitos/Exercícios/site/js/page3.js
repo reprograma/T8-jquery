4d55ff631cf6dab408352880d7c7658af3d5f3b5
Débora Borges <debora.borges@dattos.com.br>
2019-10-16 10:11:43 -0300

    Resolução dos quesitos 1 e 2 do exercício de HTML

--- 3 - Efeitos/Exercícios/site/js/page3.js
+++ 3 - Efeitos/Exercícios/site/js/page3.js
@@ -1,7 +1,36 @@
-$(function(){
-    $('button').click(function(){
+$(function () {
+
+    function configurateHover(){
+        $('.card-title').off('mouseenter').on('mouseenter',
+            function () {
+                $(this).css('color', 'yellow');
+            }
+        )
+
+        $('.card-title').off('mouseleave').on('mouseleave',
+            function () {
+                $(this).css('color', 'black');
+            }
+        )
+    }
+
+    configurateHover();
+
+    $('.btn-toggle').click(function () {
         $('.card:eq(2)').toggle(6000);
         $('.card:eq(0)').toggle(3000);
-        $('.card:eq(1)').toggle(1000);        
+        $('.card:eq(1)').toggle(1000);
     })
+
+    $('.btn-copy').click(function () {
+        let content = $(".div-original").html();
+
+        $(".div-original").after('<div class="row">' + content + '</div>');
+
+        configurateHover();
+    })
+
+
+
+
 });
